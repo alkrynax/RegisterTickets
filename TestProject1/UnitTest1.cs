194a0f0fe83977bb7bb926ea14484bbfd142658c
LELIA KOZAN <9921663S@commun.ad.sncf.fr>
2024-03-13 16:46:09 +0100

    try

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,27 @@
-namespace TestProject1
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
+
+
+public class ProgramTests
+{
+    [Fact]
+    public void TestStartApplication()
+    {
+        // Capturer la sortie console
+        using (var consoleOutput = new StringWriter())
+        {
+            Console.SetOut(consoleOutput);
+
+            // Appeler la méthode StartApplication()
+            TicketsService.StartApplication();
+
+            // Récupérer la sortie console
+            string consoleResult = consoleOutput.ToString().Trim();
+
+            // Vérifier si la sortie console contient la chaîne attendue
+            Assert.Contains("Hello, World!", consoleResult);
         }
     }
 }
